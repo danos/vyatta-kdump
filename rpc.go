danos/vyatta-kdump

commit d8c0391d1c97b9c75b6577bebdd7dddf2fb7c148

rpc: translate negative index to array index
delete-kernel-crash-dumps and get-kernel-crash-info rpc accepts negative
index to indicate its order with respect to the oldest crash dump entry.
Translate these negative indices to a proper index.

VRVDR-55771

rpc.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"github.com/danos/vyatta-kdump/internal/kdump"
 	rpc "github.com/danos/vyatta-kdump/internal/rpc"
+	"os"
 )
 
 type RPC struct {
@@ -19,9 +20,28 @@ func RPCNew(conf *Config) *RPC {
 }
 
 func (r *RPC) DeleteCrashDumps(in rpc.RPCInput) (struct{}, error) {
-	err := kdump.DelCrashDumps(in.Index)
-	if err != nil {
-		return struct{}{}, err
+	_, crashdumps := kdump.GetCrashFiles()
+	if len(in.Index) == 0 {
+		for _, dump := range crashdumps {
+			kdump.DelCrashDump(dump)
+		}
+		return struct{}{}, nil
+	}
+
+	bad_index := make([]int32, 0)
+	dumps_to_delete := make([]os.FileInfo, len(in.Index))
+	for _, index := range in.Index {
+		n, err := dumpIndex(index, len(crashdumps))
+		if err != nil {
+			bad_index = append(bad_index, index)
+		}
+		dumps_to_delete = append(dumps_to_delete, crashdumps[n])
+	}
+	if len(bad_index) != 0 {
+		return struct{}{}, fmt.Errorf("DeleteCrashDumps bad input: %v", bad_index)
+	}
+	for _, d := range dumps_to_delete {
+		kdump.DelCrashDump(d)
 	}
 	return struct{}{}, nil
 }
@@ -43,12 +63,19 @@ func (r *RPC) GetCrashDmesg(in rpc.RPCInput) (*rpc.CrashDMesgOut, error) {
 	}
 
 	for i := 0; i < len(res.CrashInfo); i++ {
-		index := res.CrashInfo[i].Index
-		if int(index) >= len(crashdumps) || int(-index) > len(crashdumps) {
+		n, err := dumpIndex(res.CrashInfo[i].Index, len(crashdumps))
+		if err != nil {
 			continue
 		}
-		res.CrashInfo[i].FileName = fmt.Sprintf("%s/%s", crash_dir, crashdumps[index].Name())
-		res.CrashInfo[i].DMesg = kdump.GetCrashDMsg(crashdumps[index])
+		res.CrashInfo[i].FileName = fmt.Sprintf("%s/%s", crash_dir, crashdumps[n].Name())
+		res.CrashInfo[i].DMesg = kdump.GetCrashDMsg(crashdumps[n])
 	}
 	return res, nil
+}
+
+func dumpIndex(n int32, ndumps int) (int, error) {
+	if int(n) >= ndumps || int(-n) > ndumps {
+		return -1, fmt.Errorf("Error: Index (%d) out of range [%d..%d]\n", n, -ndumps, ndumps-1)
+	}
+	return int(n) % ndumps, nil
 }

internal/kdump/kdump.go
@@ -352,27 +352,11 @@ func GetCrashDMsg(crashdump os.FileInfo) string {
 	return string(dmesg)
 }
 
-func DelCrashDumps(index []int32) error {
-	_, crashdumps := GetCrashFiles()
-
-	var del_list []string
-	if len(index) == 0 {
-		del_list = make([]string, len(crashdumps))
-		for i := 0; i < len(crashdumps); i++ {
-			del_list[i] = fmt.Sprintf("%s/%s", kdumpCrashDir, crashdumps[i].Name())
-		}
-	} else {
-		del_list = make([]string, len(index))
-		for i, n := range index {
-			if int(n) > len(crashdumps) || int(-n) >= len(crashdumps) {
-				return errors.New(fmt.Sprintf("Invalid Index:%d", n))
-			}
-			del_list[i] = fmt.Sprintf("%s/%s", kdumpCrashDir, crashdumps[n].Name())
-		}
-	}
-
-	for _, crash := range del_list {
-		os.RemoveAll(crash)
+func DelCrashDump(crashdump os.FileInfo) error {
+	dname := fmt.Sprintf("%s/%s", kdumpCrashDir, crashdump.Name())
+	if err := os.RemoveAll(dname); err != nil {
+		log.Ilog.Printf("DelCrashdump: %s\n", err)
+		return err
 	}
 	return nil
 }

commit d33d81b68c49b0c3a9877b4de1d32950d55c4a9a

rpc: fix DeleteCrashDumps function
This fixes a wrong array initialization introduced in commit:
d8c0391 rpc: translate negative index to array index

VRVDR-55846

rpc.go
@@ -29,13 +29,14 @@ func (r *RPC) DeleteCrashDumps(in rpc.RPCInput) (struct{}, error) {
 	}
 
 	bad_index := make([]int32, 0)
-	dumps_to_delete := make([]os.FileInfo, len(in.Index))
+	dumps_to_delete := make([]os.FileInfo, 0)
 	for _, index := range in.Index {
 		n, err := dumpIndex(index, len(crashdumps))
 		if err != nil {
 			bad_index = append(bad_index, index)
+		} else {
+			dumps_to_delete = append(dumps_to_delete, crashdumps[n])
 		}
-		dumps_to_delete = append(dumps_to_delete, crashdumps[n])
 	}
 	if len(bad_index) != 0 {
 		return struct{}{}, fmt.Errorf("DeleteCrashDumps bad input: %v", bad_index)